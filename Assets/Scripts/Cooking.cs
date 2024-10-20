361f9565ba6d5e8f1cd89c04b9a9bb926fe3e4e0
Sheikh Islam <143849374+SheikhIslamD@users.noreply.github.com>
2024-10-20 17:23:40 -0400

    Cooking system complete
    -Cooking script reworked to count up in time
    -Coroutine uses cookMin and cookMax to assign cookstates to ingredients
    -cooking status tracked live through timer and color-updating slider
    -Ingredients made to use ScriptableObjects: created Steak and Cock assets

--- Assets/Scripts/Cooking.cs
+++ Assets/Scripts/Cooking.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using Unity.Cinemachine;
@@ -6,25 +7,28 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
-/*public class Cooking : MonoBehaviour
+public class Cooking : MonoBehaviour
 {
     public TextMeshProUGUI cookingCountdownText;
     public bool currentlyCooking; //if stove is running
-    public float cookTime; //how long to cooking takes
-    public Image handsImage; //image to show hand status
+    //public float cookTime; //how long to cooking takes
+    //public Image handsImage; //image to show hand status
 
-    public bool handsFull; //if anything is held already
-    public bool holdingCookedFood; //if the item held is a completed cooked food
+    //public bool handsFull; //if anything is held already
+    //public bool holdingCookedFood; //if the item held is a completed cooked food
 
-    //public IngredientButton[] ingredientBoxes;
-    public string handsHolding; //what is held in the hands
-    public List<string> orders; //what orders there currently are (type these in inspector)
-    public string whatsCooking; //what is on the stove cooking
+    //public string handsHolding; //what is held in the hands
+    //public List<string> orders; //what orders there currently are (type these in inspector)
+    public GameObject whatsCooking; //what is on the stove cooking
 
-    public GameObject CollectCookedFood;
-    public IngredientButton ingredientButton;
+    public GameObject Pickup;
 
-    //public TextMeshProUGUI platedText;
+    public GameObject platedTransform;
+    public GameObject cookingTransform;
+
+    //new cooking system
+    public Slider timeSlider;
+    public Image timeSliderColor;
 
     //for making this a singleton
     public static Cooking instance;
@@ -36,90 +40,168 @@ using UnityEngine.UI;
             instance = this;
         }
 
-        //make hands blank
-        handsImage.color = Color.clear;
+        currentlyCooking = false;
         //hide the collect cooked food button
-        CollectCookedFood.SetActive(false);
+        Pickup.SetActive(false);
     }
-
-    void Update()
-    {
-        if (currentlyCooking) 
-        {
-            if (cookTime > 0) 
-            {
-                cookTime -= Time.deltaTime;
-                int minutes = Mathf.FloorToInt(cookTime / 60);
-                int seconds = Mathf.FloorToInt(cookTime % 60);
-                cookingCountdownText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
-            }
-            else if (cookTime < 0)
-            {
-                cookTime = 0;
-                currentlyCooking = false;
-                CollectCookedFood.SetActive(true);
-            }
-        }
-    }
-
     public void PlaceIngredient()
     {
-        if (!currentlyCooking && !holdingCookedFood)
+        //checks if something is already cooking, and if the player is holding an uncooked item
+        if (!currentlyCooking && PlayerHands.instance.heldItem.GetComponent<Ingredient>().cookState == 0)
         {
-            foreach (string order in orders)
-            {
-                if (order == handsHolding)
-                {
-                    StartCookingStove();
-                    Debug.Log("Order found, starting to cook");
-                }
-            }
+            //write the item to whatsCooking and move it to the stove, remove from player's hands
+            whatsCooking = PlayerHands.instance.heldItem;
+            PlayerHands.instance.PutDown();
+            whatsCooking.transform.SetParent(cookingTransform.transform, true);
+            whatsCooking.transform.position = cookingTransform.transform.position;
+            //begin the cooking coroutine and allow picking up the item
+            BeginCooking(0);
+            Pickup.SetActive(true);
+            Debug.Log("Starting to cook a " + whatsCooking.GetComponent<Ingredient>().IngredientName);
         }
         else
-            Debug.Log("Wrong order or Already busy cooking!");
+            Debug.Log("Can't cook that, or already cooking!");
     }
-    public void StartCookingStove()
+
+    public void BeginCooking(float cookTime)
     {
-        //redundant if check here?
-        if (!currentlyCooking)
-        {
-            whatsCooking = handsHolding;
-            handsHolding = "None";
-            handsImage.color = Color.clear;
-            cookTime = 10;
-            handsFull = false;
-            currentlyCooking = true;
-            ingredientButton.holdingThisIngredient = false;
-        }
+        timeSlider.maxValue = whatsCooking.GetComponent<Ingredient>().cookMax;
+        timeSlider.value = 0;
+        currentlyCooking = true;
+        StartCoroutine(CookingCountdown(cookTime));
     }
-    public void Plating()
+
+    public void StopCooking()
     {
-        if (holdingCookedFood)
+        currentlyCooking = false;
+        Debug.Log("Cooking Stopped");
+    }
+
+    IEnumerator CookingCountdown(float cookTime)
+    {
+        while (currentlyCooking)
         {
-            foreach (string order in orders)
+            int minutes = Mathf.FloorToInt(cookTime / 60);
+            int seconds = Mathf.FloorToInt(cookTime % 60);
+            cookingCountdownText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+            cookTime += Time.deltaTime;
+            yield return new WaitForSeconds(0.001f);
+            //keep running while curruntly cooking
+            if (currentlyCooking)
             {
-                if (order.Contains(handsHolding))
-                {
-                    handsHolding = "None";
-                    handsImage.color = Color.clear;
-                    handsFull = false;
-                    holdingCookedFood = false;
-                    Debug.Log("Correct cooked order, accepted!");
-                }
+                timeSlider.value = cookTime;
+            }
+
+            //red if undercooked
+            if (cookTime < whatsCooking.GetComponent<Ingredient>().cookMin)
+            {
+                whatsCooking.GetComponent<Ingredient>().cookState = 0;
+                timeSliderColor.color = Color.red;
+            }
+            //making the food item take on the proper cook state, if higher than min but below max then it's perfectly cooked
+            if (cookTime >= whatsCooking.GetComponent<Ingredient>().cookMin && cookTime <= whatsCooking.GetComponent<Ingredient>().cookMax)
+            {
+                whatsCooking.GetComponent<Ingredient>().cookState = 1;
+                whatsCooking.GetComponent<Ingredient>().CookUpdate();
+                timeSliderColor.color = Color.green;
+            }
+            //make it burnt if it goes to max cooktime and make currentlycooking off
+            if (cookTime > whatsCooking.GetComponent<Ingredient>().cookMax)
+            {
+                whatsCooking.GetComponent<Ingredient>().cookState = 2;
+                whatsCooking.GetComponent<Ingredient>().CookUpdate();
+                timeSliderColor.color = Color.black;
+                StopCooking();
             }
         }
     }
-    public void FinishCookingStove()
+
+    public void PickupIngredient()
     {
-        if (!currentlyCooking)
-        {
-            handsHolding = whatsCooking;
-            handsFull = true;
-            holdingCookedFood = true;
-            whatsCooking = "None";
-            handsImage.color = Color.green;
-            CollectCookedFood.SetActive(false);
-        }
+        StopCooking();
+        PlayerHands.instance.PickUp(whatsCooking);
+        whatsCooking = null;
+        Pickup.SetActive(false);
     }
+
+    public void Plating()
+    {
+        GameObject platedfood = PlayerHands.instance.heldItem;
+        PlayerHands.instance.PutDown();
+        platedfood.transform.SetParent(platedTransform.transform, true);
+        platedfood.transform.position = platedTransform.transform.position;
+    }
+
+    public void Trashcan()
+    {
+        Destroy(PlayerHands.instance.heldItem);
+        PlayerHands.instance.PutDown();
+        Debug.Log("Item trashed");
+    }
+
+    /*    void Update()
+        {
+            if (currentlyCooking) 
+            {
+                if (cookTime > 0) 
+                {
+                    cookTime -= Time.deltaTime;
+                    int minutes = Mathf.FloorToInt(cookTime / 60);
+                    int seconds = Mathf.FloorToInt(cookTime % 60);
+                    cookingCountdownText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+                }
+                else if (cookTime < 0)
+                {
+                    cookTime = 0;
+                    currentlyCooking = false;
+                    CollectCookedFood.SetActive(true);
+                }
+            }
+        }*/
+
+    /*    public void StartCookingStove()
+        {
+            //redundant if check here?
+            if (!currentlyCooking)
+            {
+                whatsCooking = handsHolding;
+                handsHolding = "None";
+                handsImage.color = Color.clear;
+                cookTime = 10;
+                handsFull = false;
+                currentlyCooking = true;
+            }
+        }*/
+    /*    public void Plating()
+        {
+            if (holdingCookedFood)
+            {
+                foreach (string order in orders)
+                {
+                    if (order.Contains(handsHolding))
+                    {
+                        handsHolding = "None";
+                        handsImage.color = Color.clear;
+                        handsFull = false;
+                        holdingCookedFood = false;
+                        Debug.Log("Correct cooked order, accepted!");
+                    }
+                }
+            }
+        }*/
+    /*    public void FinishCookingStove()
+        {
+            if (!currentlyCooking)
+            {
+                handsHolding = whatsCooking;
+                handsFull = true;
+                holdingCookedFood = true;
+                whatsCooking = "None";
+                handsImage.color = Color.green;
+                CollectCookedFood.SetActive(false);
+            }
+        }*/
+
+
 }
-*/
--- Assets/Scripts/Ingredient.cs
+++ Assets/Scripts/Ingredient.cs
@@ -1,31 +1,50 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ingredient : MonoBehaviour
 {
+    public Protein protein;
+
     [Header("Ingredient Information")]
     public string IngredientName;
 
     public float cookMin;
     public float cookMax;
 
-    public string cookMethod;
     public int cookState = 0;
 
-    public Sprite activeSprite;
+    public Sprite proteinSprite;
     [Space(10)]
 
     [Header("Dish Information")]
     public string dishName;
+    public int cookMethod;
+    public Sprite dishSprite;
+
+    public Sprite[] resultSprites;
+
+    public Image image;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        IngredientName = protein.IngredientName;
+        cookMin = protein.cookMin;
+        cookMax = protein.cookMax;
+        cookState = protein.cookState;
+        proteinSprite = protein.proteinSprite;
+
+        dishName = protein.dishName;
+        cookMethod = protein.cookMethod;
+        dishSprite = protein.dishSprite;
+        resultSprites = protein.resultSprites;
+
+        image = GetComponent<Image>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CookUpdate()
     {
-        
+        image.sprite = resultSprites[cookState];
+
     }
 }
